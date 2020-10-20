c15d0530cdfa33bd9289dcfe5ae665d6c49b0a15
Thibault Lestang <thibault.lestang@cs.ox.ac.uk>
2020-10-20 16:00:28 +0100

    Add reference implementation

--- ref/tom.py
+++ ref/tom.py
@@ -1,8 +1,9 @@
-import pybamm
+from concurrent.futures import ProcessPoolExecutor
 import numpy as np
 import matplotlib.pyplot as plt
-from concurrent.futures import ProcessPoolExecutor
+import pybamm
 import SharedArray
+from subprocess import run
 import time as time_module
 
 plt.close('all')
@@ -14,21 +15,21 @@ def current_function(t):
 if __name__ == '__main__':
    # load model
    model = pybamm.lithium_ion.SPMe()
-   
+
    # create geometry
    geometry = model.default_geometry
-   
+
    # load parameter values and process model and geometry
    param = model.default_parameter_values
-   
+
    param.update({"Current function [A]": current_function,})
    param.update({"Current": "[input]"}, check_already_exists=False)
    param.process_model(model)
    param.process_geometry(geometry)
-   
+
    # set mesh
    mesh = pybamm.Mesh(geometry, model.default_submesh_types, model.default_var_pts)
-   
+
    # discretise model
    disc = pybamm.Discretisation(mesh, model.default_spatial_methods)
    disc.process_model(model)
@@ -39,8 +40,8 @@ if __name__ == '__main__':
    solver = pybamm.CasadiSolver()
    sol_init = solver.solve(model, t_eval, inputs=inputs)
    i_app = 1.0
-   
-   Nspm = 1300
+
+   Nspm = 8
    Nsteps = 10
    try:
        shm_y = SharedArray.create('shm://y', [sol_init.y.shape[0], Nspm], dtype=float)
@@ -62,15 +63,15 @@ if __name__ == '__main__':
        shm_t[i] = 0.0
        shm_i_app[i] = i_app*(1+(i+1)/Nspm)
    #f = ex.submit(add_one, shm_key)
-   
+
    # step model
    dt = 1
    time = 0
    end_time = dt*Nsteps
    step_solver = model.default_solver
    step_solution = None
-   
-   
+
+
    def shm_step(args):
        ind, tstep = args
        shm_y = SharedArray.attach('shm://y')
@@ -85,7 +86,7 @@ if __name__ == '__main__':
        shm_y[:, ind] = step_solution.y[:, -1]
        shm_t[ind] = step_solution.t[-1]
 #        shm_V[tstep, ind] = step_solution['Terminal voltage [V]'](step_solution.t[-1])
-   
+
    step = 0
    while time < end_time:
        print('Time', time)
@@ -100,8 +101,12 @@ if __name__ == '__main__':
        step += 1
        print('Stepping time', np.around(time_module.time()-st, 2), 's')
 
-   plt.figure()
-   plt.plot(shm_V)
+
+   # Get current HEAD commint hash
+   cmd = ["git", "rev-parse", "HEAD"]
+   hash = run(cmd, capture_output=True, encoding="utf-8").stdout
+   header = f"Generated with {__file__}, revision {hash}"
+   np.savetxt("base_solution.txt", shm_y, header=header)
 
    SharedArray.delete('shm://y')
    SharedArray.delete('shm://t')
