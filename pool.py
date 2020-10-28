d3514f209fa8169861604955800f42d580455f1a
Thibault Lestang <thibault.lestang@cs.ox.ac.uk>
2020-10-28 18:47:46 +0000

    Add serial implementation and optional feedback to pool.py

--- pool.py
+++ pool.py
@@ -1,4 +1,5 @@
-from multiprocessing import Pool
+from multiprocessing import Pool, current_process
+import time
 
 import numpy as np
 
@@ -13,7 +14,16 @@ def worker(work):
     return solution
 
 
-def solve_w_pool(model, sol_init, Nsteps, dt, Nspm, processes=None):
+def worker_feedback(work):
+    st = time.time()
+    solution = work.sol_init
+    for t in range(0, work.end_time, work.dt):
+        solution = work.do_step(solution)
+
+    return (current_process().name, (time.time() - st))
+
+
+def solve_w_pool(model, sol_init, Nsteps, dt, Nspm, processes=None, serial=False, feedback=False):
     end_time = Nsteps * dt
     i_app = 1.0
     list_of_inputs = [{"Current": i_app * (1 + (i + 1) / Nspm)} for i in range(Nspm)]
@@ -21,8 +31,17 @@ def solve_w_pool(model, sol_init, Nsteps, dt, Nspm, processes=None):
         SimPool(model, sol_init, dt, end_time, list_of_inputs[ind])
         for ind in range(Nspm)
     ]
-    with Pool(processes) as p:
-        solutions = p.map(worker, work)
+
+    func = worker_feedback if feedback else worker
+
+    if serial:
+        solutions = list(map(func, work))
+    else:
+        with Pool(processes) as p:
+            solutions = p.map(func, work)
+
+    if feedback:
+        return solutions
 
     yarray = np.array([sol.y[:, -1] for sol in solutions]).transpose()
     tarray = np.array([sol.t[-1] for sol in solutions])
